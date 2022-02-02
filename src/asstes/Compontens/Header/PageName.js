73e2dd43b45606c5b438adba80b368f05023f92c
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-02-02 14:08:31 +0100

    Add changes to HeaderBranch

---
+++ src/asstes/Compontens/Header/PageName.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PageName = (props) => {
+    return <div>
+        <p className='text-white pt-6 pb-6 px-2'>{props.controlPanel} </p>
+    </div>;
+};
+
+export default PageName;
+
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Background from './asstes/Compontens/Background/Background';
+import PageName from './asstes/Compontens/Header/PageName';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <div className='bg-blue-600'>
-      <Background />
+      <div>
+        {/* <Background /> */}
+      </div>
+      <div>
+        <PageName controlPanel="ControlPanel" bedRoom="BedRoom" />
+      </div>
     </div>
 
   </React.StrictMode>,
