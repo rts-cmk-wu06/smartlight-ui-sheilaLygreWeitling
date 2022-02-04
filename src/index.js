80c7f2a30e91ec5390a1a93263a4bbe8d9db23df
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-02-04 12:49:04 +0100

    add changes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
-import Background from './asstes/Compontens/Background/Background';
-import PageName from './asstes/Compontens/Header/PageName';
-import ControlPanelBackgound from './asstes/Compontens/ControlPanel/ControlPanelBackgound';
-import Footer from './asstes/Compontens/Footer/Footer';
+
 
 
 
@@ -13,18 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <div className='bg-blue-600 w-[375px]'>
-        <div>
-          {/* <Background /> */}
-        </div>
-        <div>
-          <PageName controlPanel="ControlPanel" bedRoom="BedRoom" />
-        </div>
-        <div>
-          <ControlPanelBackgound />
-        </div>
-        <div>
-          <Footer />
-        </div>
+
       </div>
     </BrowserRouter>
   </React.StrictMode>,
