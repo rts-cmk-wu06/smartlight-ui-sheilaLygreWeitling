d50fa1fd26f585b55f192c555bc137bb180a9206
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-02-07 12:41:55 +0100

    Add changes to bedroombrach

--- src/asstes/Pages/BedRoom.js
+++ src/asstes/Pages/BedRoom.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import ChangeLights from '../Templates/ChangeLights';
 import HeaderBedroom from '../Templates/HeaderBedroom';
+import Footer from '../Compontens/Footer';
 
 
 const BedRoom = () => {
     return <div>
         <HeaderBedroom />
+        <ChangeLights />
+        <Footer />
     </div>;
 };
 
