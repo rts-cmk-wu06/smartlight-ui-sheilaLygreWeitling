b2cdbb8287e2954b10a4cece08ee0b3b43d16c38
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-02-08 11:01:23 +0100

    add changes on colorbranch

---
+++ src/asstes/Compontens/ColorButtons.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import extraBtn from '../Image/EkstraBtn.svg';
+import React from 'react';
+
+const ColorButtons = () => {
+    const ColorArray = [
+        "#FF9B9B", "#94EB9E", "#94CAEB", "#A594EB", "#DE94EB", "#EBD094",
+    ]
+
+    return <div>
+        <>
+            <h2 className="SubTitleStyle pt-[29px]">Colors</h2>
+            <div className='flex gap-x-[16px] mt-[23px]'>
+                {ColorArray.map((color) => (
+                    <button className="w-[29px] h-[29px] rounded-full" style={{ background: color }}></button>
+                ))}
+                <div className="w-[29px] h-[29px]"><img src={extraBtn} /></div>
+            </div>
+        </>
+    </div>;
+};
+
+export default ColorButtons;
+
