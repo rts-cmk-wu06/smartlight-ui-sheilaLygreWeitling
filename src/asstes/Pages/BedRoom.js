a44a9a05664e9bfa8aadc1c7c6ab2131840f3846
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-02-09 09:15:30 +0100

    Add changes to DifferentLigthsBranch

--- src/asstes/Pages/BedRoom.js
+++ src/asstes/Pages/BedRoom.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ChangeLights from '../Templates/ChangeLights';
 import HeaderBedroom from '../Templates/HeaderBedroom';
 import Footer from '../Compontens/Footer';
+import Lights from '../Templates/Lights';
 
 
 
 const BedRoom = () => {
     return <div>
         <HeaderBedroom />
+        <Lights />
         <ChangeLights />
 
         <Footer />
