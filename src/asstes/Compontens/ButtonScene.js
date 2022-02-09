d700d6957d18d5250c611ff5b2dbe204fa53ed51
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-02-09 08:52:38 +0100

    Add changes to linear gradient

--- src/asstes/Compontens/ButtonScene.js
+++ src/asstes/Compontens/ButtonScene.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LightBulb from "../Image/ligthBulb.png"
+import "../../css/App.css"
 
 const ButtonScene = () => {
     const sceneArray = [
@@ -9,15 +10,15 @@ const ButtonScene = () => {
         },
         {
             scene: "Party",
-            color: "#94EB9E"
+            color: "#A693EB"
         },
         {
             scene: "Relax",
-            color: "#94CAEB"
+            color: "#93CAEB"
         },
         {
             scene: "Fun",
-            color: "#DE94EB"
+            color: "#89DD94"
         },
     ]
 
@@ -26,7 +27,7 @@ const ButtonScene = () => {
             <h2 className="SubTitleStyle pt-[29px]">Scenes</h2>
             <div className='grid grid-cols-2 gap-x-[25px] gap-y-[19px] mt-[23px]' >
                 {sceneArray.map((theme) => (
-                    <button className="w-[150px] h-[55px] rounded-xl text-white font-bold text-sm flex items-center justify-start gap-[23px]" style={{ background: theme.color }}>
+                    <button className={`w-[150px] h-[55px] rounded-xl text-white font-bold text-sm flex items-center justify-start gap-[23px] gradient${theme.scene}`}>
                         <img src={LightBulb} alt="bulb" className='pl-[25px]' />
                         {theme.scene}</button>
                 ))}
--- src/asstes/Compontens/Footer.js
+++ src/asstes/Compontens/Footer.js
@@ -5,7 +5,7 @@ import settings from "../Image/settings.png"
 
 const Footer = () => {
     return <div>
-        <div className='bg-white flex justify-evenly p-4 min-h-[100vh]'>
+        <div className='bg-white flex justify-evenly p-4 '>
             <button>
                 <img src={bulb} alt="bulb" />
             </button>
