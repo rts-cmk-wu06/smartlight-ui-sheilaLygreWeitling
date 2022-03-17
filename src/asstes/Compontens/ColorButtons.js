85f4bc427b40be1700db5dca1891bef387e966a1
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-03-17 09:27:09 +0100

    Add changes to App.css

--- src/asstes/Compontens/ColorButtons.js
+++ src/asstes/Compontens/ColorButtons.js
@@ -2,7 +2,9 @@
 import extraBtn from '../Image/EkstraBtn.svg';
 import React from 'react';
 
+
 const ColorButtons = () => {
+
     const ColorArray = [
         "#FF9B9B", "#94EB9E", "#94CAEB", "#A594EB", "#DE94EB", "#EBD094",
     ]
@@ -10,9 +12,9 @@ const ColorButtons = () => {
     return <div>
         <>
             <h2 className="SubTitleStyle pt-[29px]">Colors</h2>
-            <div className='flex gap-x-[16px] mt-[23px]'>
+            <div className='flex gap-x-[16px] mt-[23px] '>
                 {ColorArray.map((color) => (
-                    <button className="w-[29px] h-[29px] rounded-full" style={{ background: color }}></button>
+                    <button className="w-[29px] h-[29px] rounded-full changeColor" style={{ background: color }}></button>
                 ))}
                 <div className="w-[29px] h-[29px]"><img src={extraBtn} /></div>
             </div>
