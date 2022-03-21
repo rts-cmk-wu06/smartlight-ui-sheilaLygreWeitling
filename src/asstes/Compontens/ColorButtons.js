8fff01ea9103aa2847545605ae50efba67335728
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-03-21 10:37:28 +0100

    'Add changes

--- src/asstes/Compontens/ColorButtons.js
+++ src/asstes/Compontens/ColorButtons.js
@@ -9,6 +9,7 @@ const ColorButtons = () => {
         "#FF9B9B", "#94EB9E", "#94CAEB", "#A594EB", "#DE94EB", "#EBD094",
     ]
 
+
     return <div>
         <>
             <h2 className="SubTitleStyle pt-[29px]">Colors</h2>
@@ -16,8 +17,9 @@ const ColorButtons = () => {
                 {ColorArray.map((color) => (
                     <button className="w-[29px] h-[29px] rounded-full changeColor" style={{ background: color }}></button>
                 ))}
-                <div className="w-[29px] h-[29px]"><img src={extraBtn} /></div>
+                <div className="w-[29px] h-[29px]"><img src={extraBtn} alt="extra button" /></div>
             </div>
+
         </>
     </div>;
 };
--- src/asstes/Compontens/LampSVG.js
+++ src/asstes/Compontens/LampSVG.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import Lamp from "../Image/lightbulb.svg"
+import { ReactComponent as Logo } from "../Image/bulbColorChange.svg"
+import lampNoBulb from "../Image/lampNoBulb.png"
 
 const LampSVG = () => {
+
     return <div>
-        <img src={Lamp} alt="lamp" />
+        <div className='grid grid-rows-4 grid-cols-4 pb-9 '>
+
+            <img src={lampNoBulb} alt="Lamp" className='row-start-1 col-start-1 row-end-5 col-end-5 z-10 ' />
+
+            <Logo className="lampSVG row-start-4 col-start-1 row-end-5 col-end-5 w-full mt-[19px] " />
+
+        </div>
     </div>;
 };
 
